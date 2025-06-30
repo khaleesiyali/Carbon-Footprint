568df8079b0a8cfd452d42203bc3241d3d304756
Rola Mohamed <161763403+RoM46@users.noreply.github.com>
2025-06-30 19:53:10 +0900

    Update admin.py

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,5 +1,5 @@
-from django.contrib import admin
-from .models import Appliance, UsageRecord, Category, ShoppingRecord
+om django.contrib import admin
+from .models import Appliance, UsageRecord, Category, ShoppingRecord, TravelRecord
 
 # Register your models here.
 
@@ -7,4 +7,4 @@ admin.site.register(Appliance)
 admin.site.register(UsageRecord)
 admin.site.register(Category)
 admin.site.register(ShoppingRecord)
-
+admin.site.register(TravelRecord)
